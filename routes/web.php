b79f38fa278441c2c1b3b1efdf799bcefa34c629
Juliano Berselli <bersellib@gmail.com>
2018-12-19 03:31:24 -0200

    created new routes to client.index and client.add

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/client', 'ClientController@index')->name('client.index');
+
+Route::get('/client/add', 'ClientController@add')->name('client.add');
