5cb1f9136229c6afa1434b2e5ab3d2a148d1bb37
Juliano Berselli <bersellib@gmail.com>
2018-12-19 03:28:25 -0200

    changed the navbar link home to client

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ route('client.index') }}">Client</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
